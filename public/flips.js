52430196005fc047af260f7f2d296df164e19e4f
Tatiana <sutherland.tati@gmail.com>
2017-03-27 11:59:01 -0400

    importing flips file for flip methods

---
+++ public/flips.js
@@ -0,0 +1,40 @@
+const flips = {
+    topLeftCorner: (positions) => {
+        console.log('flipping left')
+        positions[4] = 0
+        positions[5] = 1.25
+        positions[6] = 0
+        positions[7] = 0
+        positions[8] = 1.5
+        positions[9] = 0
+        positions[10] = 0
+        positions[11] = 1.5
+        positions[12] = 0
+        positions[14] = 1.25
+        positions[21] = 0
+        positions[22] = 2.75
+        positions[23] = 1.25
+        positions[75] = -2.75
+        positions[76] = 0
+        positions[77] = 1.25
+    },
+    // topRightCorner: (positions) => {
+    //     positions[0] = -5.5
+    //     positions[1] = 5.5
+    //     positions[3] = -5.5
+    //     positions[4] = 2.75
+    //     positions[6] = -2.75
+    //     positions[7] = 2.75
+    //     positions[9] = -2.75
+    //     positions[10] = 2.75
+    //     positions[12] = 0
+    //     positions[14] = 1.25
+    //     positions[21] = 0
+    //     positions[22] = 2.75
+    //     positions[23] = 1.25
+    //     positions[75] = -2.75
+    //     positions[76] = 0
+    //     positions[77] = 1.25
+    // },
+}
+
